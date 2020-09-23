9af0a13205bacaf5a0f0bed283ebbe2da5476632
YannVoytaa <wojtachjanek@gmail.com>
2020-09-23 15:44:09 +0200

    Updates

---
+++ precache-manifest.eed6a684241b3b2b4cdfdbba8e09c016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def50b4c97c87274dd0078edbcdbce2c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "f5299c60c55284d26339",
+    "url": "/resume/static/css/main.0ab0a4f2.chunk.css"
+  },
+  {
+    "revision": "5a9668df7303ef920dc1",
+    "url": "/resume/static/js/2.462782e7.chunk.js"
+  },
+  {
+    "revision": "d4b0302a1e882c57090284dfea2c6f01",
+    "url": "/resume/static/js/2.462782e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5299c60c55284d26339",
+    "url": "/resume/static/js/main.872db79e.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
